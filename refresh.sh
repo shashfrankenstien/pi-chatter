d42dbcaf2219b7a9592d6d8aeb6ec4b372fb5605
Shashank Gopikrishna <shashank.gopikrishna@gmail.com>
2016-08-30 05:34:53 -0400

    refresh.sh for chatServer

---
+++ refresh.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ $1 ]
+	then
+	echo
+	if [ -f ./.processID ]
+		then
+			pid=$(<.processID)	
+			r=$( (sudo kill "$pid") 2>&1)
+			if [ ${#r} -gt 0 ]
+				then echo "Process ID error"
+			fi
+	fi
+	echo "Starting $1 .."
+	python $1 &
+	echo $! > ./.processID
+else
+	echo "Error: Missing argument. Try './refresh.sh [server file]'"
+fi
+
+
